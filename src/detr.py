c99be0cd9f5594c584f54f44a4049d75d295cc73
hang-yin <yinhang0226@gmail.com>
2023-01-19 10:46:05 -0600

    Added CLIP experiment

--- src/detr.py
+++ src/detr.py
@@ -18,7 +18,7 @@ outputs = model(**inputs)
 # convert outputs (bounding boxes and class logits) to COCO API
 # let's only keep detections with score > 0.9
 target_sizes = torch.tensor([image.size[::-1]])
-results = processor.post_process_object_detection(outputs, target_sizes=target_sizes, threshold=0.9)[0]
+results = processor.post_process_object_detection(outputs, target_sizes=target_sizes, threshold=0.2)[0]
 
 for score, label, box in zip(results["scores"], results["labels"], results["boxes"]):
     box = [round(i, 2) for i in box.tolist()]
